e011831d976fc037638b44d1d7639c5f549ab219
Jihoon <jangna96@gmail.com>
2023-11-23 13:49:55 +0900

    setting: install apexcharts

--- src/main.js
+++ src/main.js
@@ -2,8 +2,9 @@ import { createApp } from "vue";
 import App from "./App.vue";
 import store from "./store.js";
 import { router } from "./router/index.js";
+import VueApexCharts from "vue3-apexcharts";
 
 const app = createApp(App);
 
 app.use(router);
-app.use(store).mount("#app");
+app.use(store).use(VueApexCharts).mount("#app");
